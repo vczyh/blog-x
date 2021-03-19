e9b9fafdffdb408f47600fc54bd594583d146b02
vczyh <zhangeek@qq.com>
2021-03-19 21:13:55 +0800

    update

--- src/components/MarkdownEdit.vue
+++ src/components/MarkdownEdit.vue
@@ -20,5 +20,5 @@ export default {
 </script>
 
 <style scoped>
-
+@import '~mavon-editor/dist/css/index.css';
 </style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
 import Vue from 'vue';
-import mavonEditor from 'mavon-editor';
 import App from './App.vue';
 import router from './router';
 
-import 'mavon-editor/dist/css/index.css';
-
-Vue.use(mavonEditor);
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  vuetify,
   render: (h) => h(App),
 }).$mount('#app');
